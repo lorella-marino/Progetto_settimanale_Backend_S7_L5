b58ef4f0028ab6c5a18dd29abed3d680cce16b36
lorella-marino <lorellamarino@gmail.com>
2025-05-02 16:19:17 +0200

    modifica Organizzatore

--- src/main/java/it/epicode/gestione_eventi/organizzatori/Organizzatore.java
+++ src/main/java/it/epicode/gestione_eventi/organizzatori/Organizzatore.java
@@ -1,9 +1,18 @@
 package it.epicode.gestione_eventi.organizzatori;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "organizzatori")
 public class Organizzatore {
-    private String nome;
-    private String cognome;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String nomeCompleto;
 }
