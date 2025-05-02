c3aae1461100d503a67ff37ffea72443a8f259be
lorella-marino <lorellamarino@gmail.com>
2025-05-02 15:35:51 +0200

    Initial commit

---
+++ src/test/java/it/epicode/Progetto_settimanale_Backend_S7_L5/ProgettoSettimanaleBackendS7L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.Progetto_settimanale_Backend_S7_L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanaleBackendS7L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
